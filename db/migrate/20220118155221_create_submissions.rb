dc9b8ff4a7939c8afe70fa36528e26cffbda0059
soc-man <okoth.sc@gmail.com>
2022-01-18 18:58:13 +0300

    generate submissions

---
+++ db/migrate/20220118155221_create_submissions.rb
@@ -0,0 +1,10 @@
+class CreateSubmissions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :submissions do |t|
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
